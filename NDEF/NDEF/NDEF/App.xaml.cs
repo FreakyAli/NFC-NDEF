1f7d1efb7be9e7291809bdd2e29d1ea2bc158bc4
Gulam Ali Hakim <jimtiyg@Gulams-MacBook-Pro.local>
2022-08-21 00:08:13 +0530

    Basic ui and ndef project creation

---
+++ NDEF/NDEF/NDEF/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NDEF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var navigation = new NavigationPage(new MainPage())
+            {
+                BarTextColor = Color.White,
+                BarBackgroundColor = Color.Blue,
+            };
+            MainPage = navigation;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
+
