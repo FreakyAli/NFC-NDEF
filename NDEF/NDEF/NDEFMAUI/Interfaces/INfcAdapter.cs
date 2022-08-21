9267515df98cd9796b22b41036c02a627930eb92
Gulam Ali Hakim <jimtiyg@Gulams-MacBook-Pro.local>
2022-08-21 20:42:57 +0530

    Added Android for MAUI Code

---
+++ NDEF/NDEF/NDEFMAUI/Interfaces/INfcAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NDEFMAUI.Interfaces
+{
+    public interface INfcAdapter
+    {
+        void ConfigureNfcAdapter()
+        {
+        }
+
+        void EnableForegroundDispatch()
+        {
+        }
+
+        void DisableForegroundDispatch()
+        {
+        }
+
+        void UnconfigureNfcAdapter()
+        {
+        }
+
+        Task SendAsync(List<byte[]> bytes, int delay);
+
+        Task TestNfcAsync();
+
+        Task<bool> OpenNFCSettingsAsync();
+    }
+}
+
